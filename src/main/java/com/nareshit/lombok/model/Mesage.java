ca340a086e8d297fdb77eb51a5cfee97a8ba8dcc
chintu <chintu@shradha>
2020-07-23 18:01:04 +0530

    WH-8520-First Time File Upload

---
+++ src/main/java/com/nareshit/lombok/model/Mesage.java
@@ -0,0 +1,9 @@
+package com.nareshit.lombok.model;
+
+
+public class Mesage {
+	
+	public Mesage() {
+		// TODO Auto-generated constructor stub
+	}
+}
--- src/main/java/com/nareshit/lombok/runner/MessageRunner.java
+++ src/main/java/com/nareshit/lombok/runner/MessageRunner.java
@@ -6,8 +6,9 @@ public class MessageRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("WELCOME TO SPRING BOOT APP");
+		System.out.println("HELLO MODIFIED");
 	}
 
 }
