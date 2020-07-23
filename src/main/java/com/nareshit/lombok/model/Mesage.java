dc3fafae760ff4f52e36f29d994ca6e7b44bc473
chintu <chintu@shradha>
2020-07-23 18:06:19 +0530

    WH-8520-Third Time File Upload

--- src/main/java/com/nareshit/lombok/model/Mesage.java
+++ src/main/java/com/nareshit/lombok/model/Mesage.java
@@ -1,6 +1,8 @@
 package com.nareshit.lombok.model;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class Mesage {
 	
 	public Mesage() {
--- src/main/java/com/nareshit/lombok/runner/MessageRunner.java
+++ src/main/java/com/nareshit/lombok/runner/MessageRunner.java
@@ -7,8 +7,9 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("WELCOME TO SPRING BOOT APP");
-		System.out.println("HELLO MODIFIED");
+		int a=10;
+		int b= 15;
+		 System.out.println(a+b);
 	}
 
 }
