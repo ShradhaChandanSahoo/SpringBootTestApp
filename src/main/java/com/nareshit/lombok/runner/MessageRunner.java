094a68fc977ae2bda74d6ea505ee0689eb38cbe4
chintu <chintu@shradha>
2020-07-23 17:45:13 +0530

    WH-8520-First Time File Upload

---
+++ src/main/java/com/nareshit/lombok/runner/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.nareshit.lombok.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/nareshit/lombok/SpringBootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareshit.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
